09a91ce71d4fe36111cd34039c0ec42866bedd37
simanjeremiah83-web <simanjeremiah83@gmail.com>
2026-04-22 09:30:36 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-const message = `I don’t even know if you’ll ever read this.\nBut if you do… I just want you to know, I still think about you sometimes.\n\nWe both tried. We both cared. But the universe just never gave us enough space to become “us.”\n\nI just wonder… if we had met at a different point in life, would the ending have been different?\n\nAnyway, I hope you're doing okay, even though we’re not in each other’s lives anymore.`;
+const message = `I just want to thank you for always being there by my side. On days when things aren’t easy.\nyou’re the one who makes everything feel lighter. Even if I don’t always.\n\nsay it, I truly appreciate everything you do for me.
+Thank you for your love, your understanding,.”\n\nI just wonder… if we had met at a different point in life, would the ending have been different?\n\nAnyway, I hope you're doing okay, even though we’re not in each other’s lives anymore.`;
 
 function showLetter() {
   document.getElementById("introText").style.opacity = 0;
